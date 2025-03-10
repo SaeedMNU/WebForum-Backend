efd78628a27c6e7376b4aba8280fba5ce43b45c1
Saeed Nawaz <sn1153@live.mdx.ac.uk>
2025-03-10 04:13:36 +0000

    Initial implementation of backend moved to support development in two separate repositories

---
+++ app.js
@@ -0,0 +1,40 @@
+// Necessary require/import values
+var express = require("express");
+var path = require("path");
+var app = express();
+var propertiesReader = require("properties-reader");
+var propertiesPath = path.resolve(__dirname, "conf/db.properties");
+var properties = propertiesReader(propertiesPath);
+app.use(express.json());
+const { ObjectId } = require('mongodb');
+
+var staticPath = path.join(__dirname, '..', 'WebForum-Frontend');
+app.use(express.static(staticPath));
+
+// Middleware: Log incoming requests
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
+// Referencing database connection details to access the database
+let dbPprefix = properties.get("db.prefix");
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+// Connects to the database via the connection details using the Node.js driver
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
+
+
+
+
+// Starts and logs the server start on the given port
+app.listen(3000, function () {
+    console.log("App started on port 3000");
+});
